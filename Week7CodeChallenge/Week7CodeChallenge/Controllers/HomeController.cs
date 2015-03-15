db6467d6c522c06edd87887f53fc4b7bc791c3d1
Eugene Filipovich <eugenefilipovich@email.com>
2015-03-15 17:24:56 -0600

    Day 35, Home

---
+++ Week7CodeChallenge/Week7CodeChallenge/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week7CodeChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            else { return View(); }
+        }
+        public ActionResult About()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            else { return View(); }
+        }
+        public ActionResult Work()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            else { return View(); }
+        }
+        public ActionResult Careers()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            else { return View(); }
+        }
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            Models.ContactPageForms contactForm = new Models.ContactPageForms();
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Contact(Models.ContactPageForms contactForm)
+        {
+            Models.sp6EugeneEntities db = new Models.sp6EugeneEntities();
+            db.ContactPageForms.Add(contactForm);
+            db.SaveChanges();
+            return Content("Thanks!");
+        }
+        public ActionResult Who()
+        {
+            return PartialView();
+        }
+        public ActionResult What()
+        {
+            return PartialView();
+        }
+        public ActionResult When()
+        {
+            return PartialView();
+        }
+        public ActionResult Where()
+        {
+            return PartialView();
+        }
+        public ActionResult How()
+        {
+            return PartialView();
+        }
+    }
+    }
---
+++ Week7CodeChallenge/Week7CodeChallenge/Scripts/Main.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('body').on('click', '.ajax-link', function () {
+        var url = $(this).data('url');
+        $.get(url, function (data) {
+            $('.content').html(data);
+        });
+    });
+
+    $('body').on('submit', '.ajax-form', function (event) {
+        event.preventDefault();
+        $.post($(this).data('posturl'), $(this).serialize(), function (data) {
+            $('.content').html(data);
+        });
+    });
+});
